3ece7221bb4358c33dadf5bf7c1e376866bac519
nick black <dankamongmen@gmail.com>
2024-11-04 10:59:03 -0500

    define TIMEOUT_MS

--- bme680.c
+++ bme680.c
@@ -4,6 +4,8 @@
 
 static const char* TAG = "bme";
 
+static const unsigned TIMEOUT_MS = 1000u; // FIXME why 1s?
+
 int bme680_detect(i2c_master_bus_handle_t i2c, i2c_master_dev_handle_t* i2cbme){
   const unsigned addr = 0x77;
   esp_err_t e = i2c_master_probe(i2c, addr, TIMEOUT_MS);
